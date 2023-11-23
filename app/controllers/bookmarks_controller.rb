389dfd52d3821714052cf5c4ecf263579b6fe8f7
Hongri Ma <mahongri1993@gmail.com>
2023-11-23 16:49:44 -0500

    bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,21 @@
 class BookmarksController < ApplicationController
   def new
-    # @bookmark = Bookmark.new
-    raise
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    # bookmark = Bookmark.new(params())
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get 'lists/new', to: 'lists#new'
   post 'lists', to: 'lists#create'
   get '/lists/:id', to: 'lists#show', as: :list
-  get '/lists/:list_id/bookmarks/new', to: 'bookmarks#new'
+  get '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :bookmarks
+  post '/lists/:list_id/bookmarks', to: 'bookmarks#create', as: :list_bookmarks
 end
