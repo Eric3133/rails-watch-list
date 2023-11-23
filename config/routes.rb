4c8d4f06e09330062081b0aa11aa6c9b4ed09eb9
Hongri Ma <mahongri1993@gmail.com>
2023-11-23 15:10:57 -0500

    bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get '/lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+  get '/lists/:id', to: 'lists#show', as: :list
+  get '/lists/:list_id/bookmarks/new', to: 'bookmarks#new'
 end
